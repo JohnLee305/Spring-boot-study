914ba009a7f8116c70d083799fdfa3ca7392a2a1
JohnLee305 <johnathanl305@gmail.com>
2022-03-20 21:58:18 +0900

    (2022.03.20) [1-3 : SpringBoot Spring Boot Auto Configuration]

--- src/main/java/io/jh/hello/Application.java
+++ src/main/java/io/jh/hello/Application.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 해당 어노테이션은 다음과 같은 기능을 포함함.
+//SpringBootConfiguration
+//EnableAutoConfiguration
+//ComponnetScan
+
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
