08ed09b634df53ded6110793aeb2e0af42f935a8
JohnLee305 <johnathanl305@gmail.com>
2022-03-28 21:19:53 +0900

    2022-03-28 MariaDB연동

--- src/main/java/io/jh/hello/Application.java
+++ src/main/java/io/jh/hello/Application.java
@@ -10,8 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //ComponnetScan
 
 public class Application {
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-//    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
 
 }
